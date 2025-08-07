803dd8cd28d8173298402d7f2b68242a6ed81f2b
tsiviv <t0527698098@gmail.com>
2025-08-08 01:24:42 +0300

    r

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 // יצירת חיבור למסד הנתונים
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_DB, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
   dialect: 'mysql',
-  dialectModule: require('mysql2'),
-  logging: false,
+  dialectModule: require('mysql2'),  // שימוש ב- mysql2 כדרייבר
+  logging: false // אם תרצה לכבות את הלוגים של Sequelize
 });
-
 module.exports = sequelize;
