46d5232c4bc9bab38a00c6c404a75b3859076291
tsiviv <t0527698098@gmail.com>
2025-08-08 01:38:54 +0300

    F

--- server/api.js
+++ server/api.js
@@ -67,7 +67,7 @@ app.use('/api/alternativePartmnetsRoutes', alternativePartmnetsRoutes);
 app.use('/api/MessageRoutes', MessageRoutes);
 
 // הגדרת פורט של השרת
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
